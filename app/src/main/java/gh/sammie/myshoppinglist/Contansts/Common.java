1774311c1ee0470d56206de04bfdd24a8aa305a5
sammie-gh <ofori.d.evans@gmail.com>
2019-09-30 12:30:15 +0100

    versoin 1.3

---
+++ app/src/main/java/gh/sammie/myshoppinglist/Contansts/Common.java
@@ -0,0 +1,8 @@
+package gh.sammie.myshoppinglist.Contansts;
+
+import gh.sammie.myshoppinglist.Utils.CustomToast;
+
+public class Common {
+
+    public static CustomToast Toasty = new CustomToast();
+}
